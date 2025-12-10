deebdca670ef31924833e189d1d62250e0ac8a98
Kingdavid-1234 <d.antonio1400@gmail.com>
2025-12-09 22:54:24 -0600

    Final updates: author name, fixed Class types, added f1-f4 with hotfix

--- Class2.java
+++ Class2.java
@@ -1,12 +1,21 @@
 /**
- *
- * @author Your Name
+ * @author David Ndubuisi
  */
-
 public class Class2 {
 
-    private void f1() {
-        // v1.0
+    public void f1() {
+        System.out.println("f1() called");
     }
 
+    public void f2() {
+        System.out.println("f2() called");
+    }
+
+    public void f3() {
+        System.out.println("f3() HOTFIX applied");
+    }
+
+    public void f4() {
+        System.out.println("f4() feature added");
+    }
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,41 @@
 /**
- *
- * @author Your Name
+ * @author David Ndubuisi
  */
-
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
-        System.out.println("Hello, World!");
-    }
-    
-    private void f1() {
-        // v1.0
+
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
+
+        c1.f1();
+        c2.f1();
+
+        c1.f2();
+        c2.f2();
+
+        c1.f3();
+        c2.f3();
+
+        c1.f4();
+        c2.f4();
+
+        System.out.println("Hello World!");
     }
 
+    public void f1() {
+        System.out.println("f1() called");
+    }
+
+    public void f2() {
+        System.out.println("f2() called");
+    }
+
+    public void f3() {
+        System.out.println("f3() HOTFIX applied");
+    }
+
+    public void f4() {
+        System.out.println("f4() feature added");
+    }
 }
